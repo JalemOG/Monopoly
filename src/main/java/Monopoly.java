2430b8078683b2a2d149c4cb9f81decb6a4d8e13
JalemOG <janikhamilton1441@gmail.com>
2026-09-16 10:35:50 -0600

    Se añade el .gitignore y el workflow del proyecto

---
+++ src/main/java/Monopoly.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author jalem
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
